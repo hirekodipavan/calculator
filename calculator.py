825cd89e48431666d9fa2a8fbca6f1c6ba068dd6
hirekodipavan <hirekodipavan@gmail.com>
2026-01-18 10:51:37 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+print("claculator program");
+a=int(input("enter a:"));
+b=int(input("enter b:"));
+sum=a+b
+print("sum=",sum);
+sub=a-b
+print("sub=",sub);
+mul=a*b
+print("mul=",mul);
+div=a/b
+print("div=",div)
